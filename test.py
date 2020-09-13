d77b3de2a3922c25f9a1a554a6442e3add6f9cd1
Thor Eric Dueholm <s194589@student.dtu.dk>
2020-09-13 10:02:26 +0200

    Loading data is now possible

--- test.py
+++ test.py
@@ -5,9 +5,18 @@ Created on Fri Sep  4 09:47:25 2020
 @author: yomama
 """
 
-a=4
-b=3
+import numpy as np
+import pandas as pd
 
-print(a*b)
 
-# hej med dig!
+df = pd.read_csv("http://www-stat.stanford.edu/~tibs/ElemStatLearn/datasets/SAheart.data",
+	encoding='utf-8')
+
+raw_data = df._get_values[:]
+
+
+
+
+
+
+
